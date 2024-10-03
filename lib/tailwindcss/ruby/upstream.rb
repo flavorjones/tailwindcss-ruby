6a22eedd9f8e116de078ee99321a9d8fe3e397d1
Mike Dalessio <mike.dalessio@gmail.com>
2024-10-03 12:15:23 -0400

    dep: bump tailwindcss to 4.0.0-alpha.26

--- lib/tailwindcss/ruby/upstream.rb
+++ lib/tailwindcss/ruby/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   module Ruby
     module Upstream
-      VERSION = "v4.0.0-alpha.25"
+      VERSION = "v4.0.0-alpha.26"
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
