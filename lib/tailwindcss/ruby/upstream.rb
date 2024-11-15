e181b3653a5fa702e378d9448864955ce080570e
Mike Dalessio <mike.dalessio@gmail.com>
2024-11-15 11:10:02 -0500

    dep: tailwindcss v4.0.0-alpha.34
    https://github.com/tailwindlabs/tailwindcss/releases/tag/v4.0.0-alpha.34

--- lib/tailwindcss/ruby/upstream.rb
+++ lib/tailwindcss/ruby/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   module Ruby
     module Upstream
-      VERSION = "v4.0.0-alpha.33"
+      VERSION = "v4.0.0-alpha.34"
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
