3a0a55c3c466ef76dd67ff27c2b48c8a2f9ff48e
Mike Dalessio <mike.dalessio@gmail.com>
2024-11-22 14:01:15 -0500

    dep: bump tailwindcss to v4.0.0-beta.2
    https://github.com/tailwindlabs/tailwindcss/releases/tag/v4.0.0-beta.2

--- lib/tailwindcss/ruby/upstream.rb
+++ lib/tailwindcss/ruby/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   module Ruby
     module Upstream
-      VERSION = "v4.0.0-beta.1"
+      VERSION = "v4.0.0-beta.2"
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
