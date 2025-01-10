9246374a1ef51a9ef811099c4e0d5e3a3aecae04
Mike Dalessio <mike.dalessio@gmail.com>
2025-01-10 10:23:58 -0500

    Ship separate linux-gnu and linux-musl gems
    Also backfill CI tests for aarch64-linux-* gems.

--- lib/tailwindcss/ruby/upstream.rb
+++ lib/tailwindcss/ruby/upstream.rb
@@ -9,9 +9,10 @@ module Tailwindcss
         "x64-mingw32" => "tailwindcss-windows-x64.exe",
         "x64-mingw-ucrt" => "tailwindcss-windows-x64.exe",
         "x86_64-darwin" => "tailwindcss-macos-x64",
-        "x86_64-linux" => "tailwindcss-linux-x64",
-        "aarch64-linux" => "tailwindcss-linux-arm64",
-#        "arm-linux" => "tailwindcss-linux-armv7",
+        "x86_64-linux-gnu" => "tailwindcss-linux-x64",
+        "x86_64-linux-musl" => "tailwindcss-linux-x64-musl",
+        "aarch64-linux-gnu" => "tailwindcss-linux-arm64",
+        "aarch64-linux-musl" => "tailwindcss-linux-arm64-musl",
       }
     end
   end
