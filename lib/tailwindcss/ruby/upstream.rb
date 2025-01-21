324095e425bb6ad92ddba0c4b9a5fadc54d4b20b
Chris Oliver <excid3@gmail.com>
2025-01-21 10:59:55 -0600

    Tailwind v4.0.0.beta.10

--- lib/tailwindcss/ruby/upstream.rb
+++ lib/tailwindcss/ruby/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   module Ruby
     module Upstream
-      VERSION = "v4.0.0-beta.9"
+      VERSION = "v4.0.0-beta.10"
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
