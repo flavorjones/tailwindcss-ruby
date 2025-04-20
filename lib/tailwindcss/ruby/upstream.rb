e60604e6647ae49e501638301a1260414e7ab3ce
Mike Dalessio <mike.dalessio@gmail.com>
2025-04-20 10:41:28 -0400

    dep: update to Tailwind CSS v4.1.4
    https://github.com/tailwindlabs/tailwindcss/releases/tag/v4.1.4

--- lib/tailwindcss/ruby/upstream.rb
+++ lib/tailwindcss/ruby/upstream.rb
@@ -1,7 +1,7 @@
 module Tailwindcss
   module Ruby
     module Upstream
-      VERSION = "v4.1.3"
+      VERSION = "v4.1.4"
 
       # rubygems platform name => upstream release filename
       NATIVE_PLATFORMS = {
